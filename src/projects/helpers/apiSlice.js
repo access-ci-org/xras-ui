433229c4bd51e7f6488843de546028ba0873809b
Rebecca Eveland <reveland@illinois.edu>
2024-10-03 16:42:20 -0500

    Reworking the corrections check

--- src/projects/helpers/apiSlice.js
+++ src/projects/helpers/apiSlice.js
@@ -809,7 +809,7 @@ export const apiSlice = createSlice({
         state.projectsList = action.payload.projectsList.map((project) => {
           const { grantNumber, requestMasterId, requests, status, title } =
             project;
-          const returnedForCorrections = project.requests.filter(r => r.actions.filter(a => a.returnedForCorrections));
+          const returnedForCorrections = project.requests.filter(r => r.actions.filter(a => a.returnedForCorrections).length > 0).length > 0
           const projectStatus =
             returnedForCorrections ? "Returned for Corrections" :
             status ||
