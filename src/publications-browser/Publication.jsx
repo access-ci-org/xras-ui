9e923fc3920f068c6aa24f45e5cb55f3108280eb
Rebecca Eveland <reveland@illinois.edu>
2025-02-27 23:31:47 -0600

    style: Tweaks to publication display

--- src/publications-browser/Publication.jsx
+++ src/publications-browser/Publication.jsx
@@ -1,120 +1,80 @@
-import {useState} from "react";
-import Accordion from "react-bootstrap/Accordion";
-
 const Publication = ({publication}) => {
-  const [showAllAuthors, setShowAllAuthors] = useState(false);
-
-  const getAuthorsToShow = () => {
-    if (showAllAuthors) {
-      return publication.authors;
-    }
-
-    return publication.authors.slice(0, 3);
-  }
-
-  const handleClick = () => {
-    setShowAllAuthors(!showAllAuthors);
-  }
-
-  const authorsToDisplay = getAuthorsToShow
-
-  const shouldShowEllipses = (index) => {
-    const isLastAuthor = index === authorsToDisplay().length - 1
-    const hasMoreAuthors = publication.authors.length > 3
-    const isNotShowingAll = !showAllAuthors
-
-    return isLastAuthor && hasMoreAuthors && isNotShowingAll
-  }
 
   const cleanDOI = (doi) => {
     return doi.startsWith("https") ? doi.split("/").slice(3) : doi
   }
 
-  return (
-    <div className="card mb-4">
-      <div className="card-header bg-primary text-white">
-        <span className="fw-bold">{publication.title}</span>
-        <br/>
-        <span className="fst-italic">
-            {authorsToDisplay().map((author, index) => (
-              <small key={`author_${index}`}>
-                {author.first_name} {author.last_name}
-                {index < authorsToDisplay().length - 1 && ", "}
-                {shouldShowEllipses(index) ? "..." : ""}
-              </small>
-            ))}
-          <>
-            {publication.authors.length > 3 && (
-              <button onClick={handleClick}
-                      className="btn"
-                      style={{textTransform: "lowercase", textDecoration: "none", color: "white", backgroundColor: "inherit", border: "none"}}>
-                {showAllAuthors ? "(See Less)" : "(See More)"}
-              </button>
-            )}
-          </>
-        </span>
-      </div>
-      <div className="card-body">
-        <div className="row fw-bold border-bottom">
+  const fields = ["Publication Type", "Publication Year", "DOI", "Journal" ];
+
+  const getFieldContent = (field) => {
+    switch (field) {
+      case "Publication Type":
+        return publication.publication_type;
+      case "Publication Year":
+        return publication.publication_year ? publication.publication_year : "N/A";
+      case "DOI":
+        if(!publication.doi) return false;
+        return(
+            <a
+              href={`https://doi.org/${publication.doi}`}
+              target="_blank"
+              className='fw-normal'
+            >
+              {cleanDOI(publication.doi)}
+            </a>
+        )
+      case "Journal":
+        return publication.journal.title ? publication.journal.title : false;
+    }
+  }
+
+  const renderField = (field) => {
+    const content = getFieldContent(field);
+    if(!content) return "";
+
+    return (
+      <div className="col-12 col-md-6 mb-2" key={`field_${field}_${publication.publication_id}`}>
+        <div className="row fw-bold">
           <div className="col">
-            <span className="mb-1 pb-0 tooltip-underline">Publication Type</span>
-          </div>
-          <div className="col">
-            <span className="mb-1 pb-0">Publication Year</span>
+            <span className="mb-1 pe-3 border-bottom">{field}</span>
           </div>
         </div>
         <div className="row">
           <div className="col">
-            {publication.publication_type}
-          </div>
-          <div className="col">
-            {publication.publication_year ? publication.publication_year : "N/A"}
+            <span className="mb-1 pb-0">
+              { content }
+            </span>
           </div>
         </div>
-        {(publication.doi || publication.journal.title) && (
-          <Accordion flush className="mt-3 mb-1">
-            <Accordion.Item eventKey="0">
-              <Accordion.Header>
-                Additional Information
-              </Accordion.Header>
-              <Accordion.Body>
-                <div className="row fw-bold border-bottom">
-                  {publication.doi && (
-                    <div className="col">
-                      <span className="mb-1 pb-0">DOI</span>
-                    </div>
-                  )}
-                  {publication.journal.title && (
-                    <div className="col">
-                      <span className="mb-1 pb-0">Journal</span>
-                    </div>
-                  )}
-                </div>
-                <div className="row">
-                  {publication.doi && (
-                    <div className="col">
-                      <a
-                        href={`https://doi.org/${publication.doi}`}
-                        target="_blank"
-                        className='text-decoration-none'
-                      >
-                        {cleanDOI(publication.doi)}
-                      </a>
-                    </div>
-                  )}
-                  <div className="col">
-                    {publication.journal.title && (
-                      <div className="col">
-                        {publication.journal.title}
-                      </div>
-                    )}
-                  </div>
-                </div>
-              </Accordion.Body>
-            </Accordion.Item>
+      </div>
+    )
+  }
 
-          </Accordion>
-        )}
+  const authors = publication.authors
+    .map((author) => `${author.last_name}, ${author.first_name}`)
+    .join(',')
+
+  return (
+    <div className="col-12 mb-2">
+      <div className="card" style={{ borderRadius: '0.375rem' }}>
+        <div className="card-header bg-primary text-white">
+          <span className="fw-bold">{publication.title}</span>
+        </div>
+        <div className="card-body pt-2">
+          <div className="row fw-bold">
+            <div className="col">
+              <span className="mb-1 pb-0 pe-3 border-bottom">Authors</span>
+            </div>
+          </div>
+          <div className="row mb-2">
+            <div className="col">
+              { authors }
+            </div>
+          </div>
+          <div className="row">
+            { fields.map((field) => renderField(field) )}
+          </div>
+        </div>
       </div>
     </div>
   )
