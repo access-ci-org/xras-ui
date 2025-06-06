8bef2c045c1cfa04194e89c6aabde37b3918e13b
Matt Yoder <mjyoder@psc.edu>
2025-06-06 11:43:09 -0400

    build(vite): replace NODE_ENV in output

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   define: {
-    'process.env': {}
+    "process.env": {},
+    "process.env.NODE_ENV": JSON.stringify(mode),
   },
   build: {
     cssCodeSplit: true,
@@ -30,4 +31,4 @@ export default defineConfig({
     },
   },
   plugins: [react()],
-});
+}));
