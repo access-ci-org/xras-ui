180397c049d43b9c18f70a49d83ad35e008516de
Matt Yoder <mjyoder@psc.edu>
2025-08-14 11:42:09 -0400

    feat(publications-browser): display tags horizontally under citation

--- src/publications-browser/Publication.jsx
+++ src/publications-browser/Publication.jsx
@@ -1,44 +1,37 @@
 import PublicationCitation from "./PublicationCitation";
+import style from "./Publication.module.css";
 
-const citationStyle = {
-  textIndent: "-50px",
-  marginLeft: "50px",
-};
-
-const Publication = ({ publication, index }) => {
+const Publication = ({ publication }) => {
   const { projects, publication_type: pubType } = publication;
   const grant_numbers = projects.map((project) => project.grant_number);
-  const bgColor = index % 2 == 0 ? "bg-light" : "";
 
   return (
     <div className="col-12 mb-2 border-bottom">
       <div className="card" style={{ border: "unset" }}>
-        <div className={`card-body pt-2 mb-2 ${bgColor}`}>
-          <p style={citationStyle}>
+        <div className="card-body pt-2">
+          <div className={style.citation}>
             <PublicationCitation publication={publication} />
-          </p>
-          {projects.length > 0 && (
-            <>
-              <p className="text-decoration-underline mb-2">
-                Projects Supporting this{" "}
-                {pubType === "Other" ? "Publication" : pubType}
-              </p>
-              <ul style={{ ...citationStyle, listStyleType: "none" }}>
-                {grant_numbers.map((grant, index) => (
-                  <li key={`grant_${index}`} id={`project_${index}`}>
-                    <a
-                      href={`https://allocations.access-ci.org/current-projects?_requestNumber=${grant}`}
-                      id={`grant_link${index}`}
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      {grant}
-                    </a>
-                  </li>
-                ))}
-              </ul>
-            </>
-          )}
+          </div>
+          <ul className={style.tags}>
+            <li key="project-type">
+              <i className="bi bi-file-earmark"></i>
+              {pubType === "Other" ? "Publication" : pubType}
+            </li>
+            {grant_numbers.map((grant, index) => (
+              <li key={`grant_${index}`} id={`project_${index}`}>
+                <a
+                  href={`https://allocations.access-ci.org/current-projects?_requestNumber=${grant}`}
+                  id={`grant_link${index}`}
+                  target="_blank"
+                  rel="noreferrer"
+                  title={`Supported by project ${grant}`}
+                >
+                  <i className="bi bi-link-45deg"></i>
+                  {grant}
+                </a>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </div>
