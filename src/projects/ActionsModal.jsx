e924b7894d47ce1dc30349af19e1052e5e836ea3
Steven Peckins <speckins@illinois.edu>
2026-02-03 13:37:15 -0600

    Build manage project actions incrementally
    Instead of defining the "actions" array inline, create it empty and push
    elements on.  This will make it easier to conditionally skip addition of
    options, as we'll be doing for supplements when an upgrade is available.

--- src/projects/ActionsModal.jsx
+++ src/projects/ActionsModal.jsx
@@ -37,6 +37,8 @@ export default function ActionsModal({ requestId, grantNumber }) {
   // Explore, Discover, Accelerate.
   renewalActions.sort((a, b) => (a.opportunityId < b.opportunityId ? -1 : 1));
 
+  const actions = [];
+
   // Retrieve % of used resource
   let usedPercent = getResourceUsagePercent(request);
 
@@ -88,134 +90,135 @@ export default function ActionsModal({ requestId, grantNumber }) {
     };
   }
 
-  const actions = [
-    {
-      id: "extension",
-      action: `${newActionPath}?action_type=Extension`,
-      isEnabled: "Extension" in request.allowedActions,
-      button: "Request an Extension",
-      enabled: request.usesCredits ? (
-        <p>
-          Need more time to use your current credits and allocations? You can
-          extend your project's end date up to six months past your funding end
-          date, or up to 12 months at a time for projects not supported by a
-          funding award.
-        </p>
-      ) : (
-        <p>
-          Need more time to use your current allocations? A one-time extension
-          can extend your project end date by up to six months.
-        </p>
-      ),
-      disabled: (
-        <p>
-          Your project is not currently eligible for an extension of its end
-          date.
-          {request.usesCredits ? (
-            <>
-              {" "}
-              Extensions can be requested starting 90 days before your project's
-              current end date.
-            </>
-          ) : null}
-        </p>
-      ),
-    },
-    {
-      id: "supplement",
-      action: `${newActionPath}?action_type=Supplement`,
-      isEnabled: "Supplement" in request.allowedActions,
-      button: "Request a Supplement",
-      enabled: request.usesCredits ? (
-        <p>
-          Need more credits to complete your project? Great news! Your{" "}
-          {request.allocationType} project is eligible for a supplement of{" "}
+  actions.push({
+    id: "extension",
+    action: `${newActionPath}?action_type=Extension`,
+    isEnabled: "Extension" in request.allowedActions,
+    button: "Request an Extension",
+    enabled: request.usesCredits ? (
+      <p>
+        Need more time to use your current credits and allocations? You can
+        extend your project's end date up to six months past your funding end
+        date, or up to 12 months at a time for projects not supported by a
+        funding award.
+      </p>
+    ) : (
+      <p>
+        Need more time to use your current allocations? A one-time extension
+        can extend your project end date by up to six months.
+      </p>
+    ),
+    disabled: (
+      <p>
+        Your project is not currently eligible for an extension of its end
+        date.
+        {request.usesCredits ? (
+          <>
+            {" "}
+            Extensions can be requested starting 90 days before your project's
+            current end date.
+          </>
+        ) : null}
+      </p>
+    ),
+  });
+
+  actions.push({
+    id: "supplement",
+    action: `${newActionPath}?action_type=Supplement`,
+    isEnabled: "Supplement" in request.allowedActions,
+    button: "Request a Supplement",
+    enabled: request.usesCredits ? (
+      <p>
+        Need more credits to complete your project? Great news! Your{" "}
+        {request.allocationType} project is eligible for a supplement of{" "}
+        {
           {
-            {
-              Explore: "200,000",
-              Discover: "750,000",
-              Accelerate: "1,500,000",
-            }[request.allocationType]
-          }{" "}
-          additional ACCESS credits.
-        </p>
-      ) : (
-        <p>
-          Need more units to complete your research? Great news! Your{" "}
-          {request.allocationType} project is eligible for a supplement of
-          additional units.
-        </p>
-      ),
-      disabled: (
-        <p>
-          Your project is not currently eligible for a supplement of additional{" "}
-          {request.usesCredits ? "ACCESS Credits" : "units"}.
-        </p>
-      ),
-    },
-    {
-      id: "renewal",
-      action: renewalActions.map((action) => [
-        action.opportunityName,
-        `${config.routes.renew_request_path(requestId)}?opportunity_id=${
-          action.opportunityId
-        }`,
-        "post",
-      ]),
-      isEnabled: renewalAllowed,
-      button: "Request a Renewal",
-      enabled: (
-        <p>
-          Your {request.allocationType} project can be renewed! The requirements
-          for renewing your project depend on the{" "}
-          <a href={config.routes.project_types_path()} target="_blank">
-            new project type you select
-          </a>
-          .
-        </p>
-      ),
-      disabled: (
-        <p>
-          Your project is not currently eligible for renewal.
-          {request.usesCredits && (
-            <>
-              {" "}
-              Renewals are available starting 30 days before the project's
-              current end date.
-            </>
-          )}
-          {request.isMaximize && (
-            <>
-              {" "}
-              You can submit a renewal request to the Maximize ACCESS
-              opportunity closest to your project&apos;s current end date.
-            </>
-          )}
-        </p>
-      ),
-    },
-    {
-      id: "help",
-      action: [
-        ["Learn How to Manage Allocations", config.routes.how_to_path(), "get"],
-        [
-          "Open a Help Ticket",
-          "https://support.access-ci.org/open-a-ticket",
-          "get",
-        ],
+            Explore: "200,000",
+            Discover: "750,000",
+            Accelerate: "1,500,000",
+          }[request.allocationType]
+        }{" "}
+        additional ACCESS credits.
+      </p>
+    ) : (
+      <p>
+        Need more units to complete your research? Great news! Your{" "}
+        {request.allocationType} project is eligible for a supplement of
+        additional units.
+      </p>
+    ),
+    disabled: (
+      <p>
+        Your project is not currently eligible for a supplement of additional{" "}
+        {request.usesCredits ? "ACCESS Credits" : "units"}.
+      </p>
+    ),
+  });
+
+  actions.push({
+    id: "renewal",
+    action: renewalActions.map((action) => [
+      action.opportunityName,
+      `${config.routes.renew_request_path(requestId)}?opportunity_id=${
+        action.opportunityId
+      }`,
+      "post",
+    ]),
+    isEnabled: renewalAllowed,
+    button: "Request a Renewal",
+    enabled: (
+      <p>
+        Your {request.allocationType} project can be renewed! The requirements
+        for renewing your project depend on the{" "}
+        <a href={config.routes.project_types_path()} target="_blank">
+          new project type you select
+        </a>
+        .
+      </p>
+    ),
+    disabled: (
+      <p>
+        Your project is not currently eligible for renewal.
+        {request.usesCredits && (
+          <>
+            {" "}
+            Renewals are available starting 30 days before the project's
+            current end date.
+          </>
+        )}
+        {request.isMaximize && (
+          <>
+            {" "}
+            You can submit a renewal request to the Maximize ACCESS
+            opportunity closest to your project&apos;s current end date.
+          </>
+        )}
+      </p>
+    ),
+  });
+
+  actions.push({
+    id: "help",
+    action: [
+      ["Learn How to Manage Allocations", config.routes.how_to_path(), "get"],
+      [
+        "Open a Help Ticket",
+        "https://support.access-ci.org/open-a-ticket",
+        "get",
       ],
-      isEnabled: true,
-      button: "Request Help",
-      enabled: (
-        <p>
-          Need to change something else about your project or expecting another
-          option? Submit a help ticket that includes{" "}
-          <em>grant number {project.grantNumber}</em> and a detailed description
-          of the problem.
-        </p>
-      ),
-    },
-  ];
+    ],
+    isEnabled: true,
+    button: "Request Help",
+    enabled: (
+      <p>
+        Need to change something else about your project or expecting another
+        option? Submit a help ticket that includes{" "}
+        <em>grant number {project.grantNumber}</em> and a detailed description
+        of the problem.
+      </p>
+    ),
+  });
 
   if (upgradeAllowed) {
       actions.push(upgradeAction);
