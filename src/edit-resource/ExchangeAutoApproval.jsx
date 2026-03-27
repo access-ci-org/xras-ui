49fe29efca147e49ce148f3dd3b0740fb96e5211
Nathan Tolbert <tolbert@illinois.edu>
2026-03-27 09:39:15 -0500

    Remove auto_approve_exchanges field
    The previous iteration used two fields to track the
    auto_approve_exchange state, when one is sufficient,
    considering 0 as a "do not auto-approve" state.

--- src/edit-resource/ExchangeAutoApproval.jsx
+++ src/edit-resource/ExchangeAutoApproval.jsx
@@ -12,31 +12,19 @@ export const ExchangeAutoApproval = React.memo(function ExchangeRatesGrid({
     <div>
       <h2>Auto-approve Exchanges</h2>
       <small className="form-text help-block">
-        If auto-approval is enabled, then any Exchange request for your resource,
+        If auto-approval is enabled, by setting this value to any number greater than 0, then any Exchange request for your resource,
         where the project has already had an approved exchange in the past,
         will be automatically approved if it is less than or equal to the resource limit specified.
       </small>
       <div>
-        <label className="checkbox">
-          <input type="checkbox"
-                 id="auto-approve-exchange"
-                 name="auto-approve-exchange"
-                 checked={resourceDetails.auto_approve_exchanges === true}
-                 onChange={(e) =>
-                   dispatch(updateResourceField("auto_approve_exchanges", e.target.checked))
-                 }
-          />
-          Auto-approve subsequent exchanges
-        </label>
         <TextInput
-            label="Limit auto-approval to exchanges less than or equal to:"
+            label="Auto-approve exchanges less than or equal to:"
             value={resourceDetails.auto_approve_exchange_limit}
             onChange={(e) =>
                 dispatch(updateResourceField("auto_approve_exchange_limit", e.target.value))
             }
             type="number"
             inputClassName="span4"
-            disabled={!resourceDetails.auto_approve_exchanges}
             inputAddon={unitTypesOptions.find(opt => opt.value == [resourceDetails.unit_type_id])?.label}
 
          />
